aafe0c879cf7aac2d22b62540031ddec2c5d3531
Shadab Khan <realshadabkhan@gmail.com>
2020-10-20 13:58:02 +0530

    Create AddTwoIntegers.java

---
+++ Java/Basic/Add Two Number/AddTwoIntegers.java
@@ -0,0 +1,12 @@
+import javax.print.event.PrintJobListener;
+
+public class AddTwoIntegers{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("Enter Number is" + " " + a + " " + b);
+        int sum = a + b;
+        System.out.println("The sum of two number is" + " " + sum);
+
+    }
+}
